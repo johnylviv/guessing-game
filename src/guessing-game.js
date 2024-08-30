ee3c00012d4cb47dc3d5a95585ef42a793414bb1
Yevheniy Hordiyenko <johnylviv@gmail.com>
2024-08-30 16:23:38 +0300

    feat: solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        // Округляем вниз для целого числа
+        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        // Если текущее предположение меньше, то текущее число становится максимумом
+        this.max = this.currentGuess;
     }
 
     greater() {
-
+        // Если текущее предположение больше, то текущее число становится минимумом
+        this.min = this.currentGuess;
     }
 }
 
